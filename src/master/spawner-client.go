07df2a27889422bfc71b75cae931cc572608ae66
josh <joshbothun@gmail.com>
2019-08-19 22:11:17 -0700

    Allow the spawner and master services to communicate over docker network

--- src/master/spawner-client.go
+++ src/master/spawner-client.go
@@ -10,14 +10,19 @@ import (
 	"google.golang.org/grpc"
 )
 
+const (
+	// Use the docker service name.
+	SPAWNER_ADDRESS = "spawner:3001"
+)
+
 func SendTestSpawnerRequest() {
 	var opts []grpc.DialOption
 	opts = append(opts, grpc.WithInsecure())
 
-	conn, err := grpc.Dial("localhost:3001", opts...)
+	conn, err := grpc.Dial(SPAWNER_ADDRESS, opts...)
 
 	if err != nil {
-		log.Printf("Error connecting to RPC host: %v", err)
+		log.Printf("Error connecting to RPC host %v: %v", SPAWNER_ADDRESS, err)
 		return
 	}
 	defer conn.Close()
