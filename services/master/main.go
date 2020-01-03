449f0bc54d722c9f896a06b2a8ec1b0ccfebe849
josh <joshbothun@gmail.com>
2020-01-03 10:00:40 -0800

    Fix master initialization

--- services/master/main.go
+++ services/master/main.go
@@ -1,23 +1,25 @@
 package main
 
 import (
-	// "google.golang.org/grpc"
-	// "minornine.com/hotel/src/proto"
 	"flag"
 	"fmt"
-  "log"
-  // "net"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
 	"net/http"
 	"os"
 
 	"github.com/gorilla/handlers"
 	"minornine.com/hotel/src/master"
+	hotel_pb "minornine.com/hotel/src/proto"
 	"minornine.com/hotel/src/shared"
 )
 
 const (
 	DEFAULT_HTTP_PORT = 3000
-	DEFAULT_RPC_PORT = 3001
+	DEFAULT_RPC_PORT  = 3001
 )
 
 var dataPath = shared.GetEnv("HOTEL_DATA_PATH", ".")
@@ -34,25 +36,25 @@ func main() {
 	master.StartReaper(config, store)
 
 	// Start the HTTP server in a goroutine.
-	addr := fmt.Sprintf(":%v", DEFAULT_HTTP_PORT)
+	httpAddr := fmt.Sprintf(":%v", DEFAULT_HTTP_PORT)
 	mainRouter := handlers.LoggingHandler(os.Stdout, master.CreateRouter(store))
-	log.Println("Running HTTP server on", addr)
+	log.Println("Running HTTP server on", httpAddr)
 	go func() {
-		log.Fatal(http.ListenAndServe(addr, mainRouter))
+		log.Fatal(http.ListenAndServe(httpAddr, mainRouter))
 	}()
 
-  // Start the RPC server in a goroutine.
-  // addr = fmt.Sprintf(":%v", DEFAULT_RPC_PORT)
-  // tcpListener, err := net.Listen("tcp", addr)
-  // if err != nil {
-  //   panic(fmt.Sprintf("Error binding TCP socket to %v", addr))
-  // }
-  // grpcServer := grpc.NewServer()
-  // hotel_pb.RegisterMasterServiceServer(grpcServer, &master.MasterService{})
-  // log.Println("Running RPC server on", addr)
-  // go func() {
-  //   log.Fatal(grpcServer.Serve(tcpListener))
-  // }()
+	// Start the RPC server in a goroutine.
+	rpcAddr := fmt.Sprintf(":%v", DEFAULT_RPC_PORT)
+	tcpListener, err := net.Listen("tcp", rpcAddr)
+	if err != nil {
+		panic(fmt.Sprintf("Error binding TCP socket to %v", rpcAddr))
+	}
+	grpcServer := grpc.NewServer()
+	hotel_pb.RegisterMasterServiceServer(grpcServer, &master.MasterService{})
+	log.Println("Running RPC server on", rpcAddr)
+	go func() {
+		log.Fatal(grpcServer.Serve(tcpListener))
+	}()
 
 	// Setup a SIGINT (CTRL+C) shutdown signal and block on it.
 	c := shared.CreateSigintChannel()
