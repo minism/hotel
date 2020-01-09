097c0bd2462bd9cda7a17ef42aae96d4102f3415
josh <joshbothun@gmail.com>
2020-01-09 12:48:32 -0800

    Make port configurable

--- services/spawner/main.go
+++ services/spawner/main.go
@@ -14,7 +14,6 @@ import (
 )
 
 const (
-	DEFAULT_PORT           uint32 = 3002
 	MASTER_CONTACT_RETRIES        = 5
 )
 
@@ -32,13 +31,11 @@ func main() {
 
 	// Initialize main components.
 	config := spawner.LoadConfig(configPath)
-	config.Port = DEFAULT_PORT
 	controller := spawner.NewServerController(&config)
 	service := spawner.NewSpawnerService(&config, controller)
 
 	// Start the RPC server in a goroutine.
-	port := DEFAULT_PORT
-	addr := fmt.Sprintf(":%v", port)
+	addr := fmt.Sprintf(":%v", config.Port)
 	listener, err := net.Listen("tcp", addr)
 	if err != nil {
 		panic(fmt.Sprintf("Error binding TCP socket to %v", addr))
@@ -54,7 +51,7 @@ func main() {
 	go func() {
 		masterClient := spawner.NewMasterClient(masterServerAddress)
 		for i := 0; i < MASTER_CONTACT_RETRIES; i++ {
-			err := masterClient.Register(port, service.GetStatus())
+			err := masterClient.Register(config.Port, service.GetStatus())
 			if err == nil {
 				return
 			}
--- src/spawner/config.go
+++ src/spawner/config.go
@@ -4,6 +4,8 @@ import (
 	"minornine.com/hotel/src/shared"
 )
 
+const DEFAULT_PORT uint32 = 3002
+
 // Config contains global configuration for the hotel spawner instance.
 // The configuration is loaded from a JSON file provided at runtime.
 type Config struct {
@@ -21,17 +23,21 @@ type Config struct {
 	// However, for a single docker network stack, this is a bad idea because
 	// the master server will discover the docker IP for the spawner rather than
 	// the public IP. So, the best practice is to always set this.
-	Host string
+	Host string `json:"host"`
 
-	// Port that the spawner is running on, not loaded from the config file.
-	// At the moment this is hardcoded since we expect the spawner process
-	// to be ran inside a docker container, where the port can be remapped.
-	Port uint32
+	// Port that the spawner is running on.
+	Port uint32 `json:"port"`
 }
 
 // LoadConfig loads and returns a spawner config from the given path.
 func LoadConfig(configPath string) Config {
 	var config Config
 	shared.LoadConfigFromPath(configPath, &config)
+
+	// Apply missing defaults.
+	if config.Port == 0 {
+		config.Port = DEFAULT_PORT
+	}
+
 	return config
 }
